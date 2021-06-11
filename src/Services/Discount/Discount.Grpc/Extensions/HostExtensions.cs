b5c2d2100cf553fb5a0965ce544a96e1708bfe48
Eduardo Medina <eduardo@soldsie.com>
2021-06-11 17:34:52 -0500

    Added Discount Grpc Service

---
+++ src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Migrating postgresql database");
+
+                    var dbConnectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+                    using var connection = new NpgsqlConnection(dbConnectionString);
+                    connection.Open();
+
+                    //the DB is already created using the connection string.
+
+                    using var command = new NpgsqlCommand { Connection = connection };
+                    command.CommandText = "DROP TABLE IF EXISTS Coupons";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE Coupons(Id SERIAL PRIMARY KEY,
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"INSERT INTO Coupons(ProductName,Description, Amount) 
+                                                        VALUES('Iphone X', 'Iphone Discount', 150);";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"INSERT INTO Coupons(ProductName,Description, Amount) 
+                                                        VALUES('Samsung 10', 'Samsung Discount', 100);";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("Migrated postgresql database");
+                }
+                catch (Exception ex)
+                {
+
+                    logger.LogError(ex, "Error while migrating the postgresql database");
+                    if (retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        System.Threading.Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host,retryForAvailability);
+                    }
+                }
+
+                return host;
+            }
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Repositories/DiscountRepository.cs
@@ -0,0 +1,78 @@
+﻿using Dapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Repositories.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _dbConnectionString;
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _dbConnectionString = _configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+        }
+
+        public async Task<Coupon> GetDiscountAsync(string productName)
+        {
+
+            try
+            {
+            using var connection = new NpgsqlConnection(_dbConnectionString);
+                var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+                    ("SELECT * FROM Coupons WHERE ProductName = @ProductName", new { ProductName = productName });
+
+                return coupon ?? new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+            }
+            catch (Exception ex)
+            {
+
+                var m = ex.Message;
+            }
+
+            return null;
+
+        }
+
+        public async Task<bool> CreateDiscountAsync(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection(_dbConnectionString);
+
+            var affected = await connection.ExecuteAsync
+                ("INSERT INTO Coupons (ProductName,Description,Amount) VALUES (@ProductName, @Description, @Amount)",
+                new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+
+            return affected > 0;
+        }
+        public async Task<bool> UpdateDiscountAsync(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection(_dbConnectionString);
+
+            var affected = await connection.ExecuteAsync
+                ("UPDATE Coupons SET ProductName=@ProductName, Description=@Description, Amount=@Amount WHERE Id = @Id",
+                new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount, Id=coupon.Id });
+
+            return affected > 0;
+        }
+
+        public async Task<bool> DeleteDiscountAsync(string productName)
+        {
+            using var connection = new NpgsqlConnection(_dbConnectionString);
+
+            var affected = await connection.ExecuteAsync
+                ("DELETE FROM Coupons WHERE ProductName = @ProductName",
+                new { ProductName = productName });
+
+            return affected > 0;
+        }
+
+    }
+}
