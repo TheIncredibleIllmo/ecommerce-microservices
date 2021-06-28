ec2b41a8730e4490a4125cb3c12f567c447066c1
Eduardo Medina <eduardo@soldsie.com>
2021-06-28 15:30:15 -0500

    Added: Discount gRPC service

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories.Interfaces;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _discountRepo;
+        private readonly IMapper _mapper;
+
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository discountRepo, ILogger<DiscountService> logger, IMapper mapper)
+        {
+            _discountRepo = discountRepo ?? throw new ArgumentException(nameof(discountRepo));
+            _logger = logger ?? throw new ArgumentException(nameof(logger));
+            _mapper = mapper ?? throw new ArgumentException(nameof(mapper));
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _discountRepo.GetDiscountAsync(request.ProductName);
+            if (coupon == null) throw new RpcException(new Status(StatusCode.NotFound,$"Discount with ProductName= {request.ProductName} was not found."));
+
+            _logger.LogInformation($"Discount retrieved for ProductName: {coupon.ProductName}, Amount: {coupon.Amount}");
+
+            return _mapper.Map<CouponModel>(coupon);
+        }
+
+    }
+}
--- src/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Discount.Grpc.Repositories;
 using Discount.Grpc.Repositories.Interfaces;
+using Discount.Grpc.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -20,6 +21,7 @@ namespace Discount.Grpc
         {
             services.AddScoped<IDiscountRepository, DiscountRepository>();
             services.AddGrpc();
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +36,7 @@ namespace Discount.Grpc
 
             app.UseEndpoints(endpoints =>
             {
-                //endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<DiscountService>();
 
                 endpoints.MapGet("/", async context =>
                 {
