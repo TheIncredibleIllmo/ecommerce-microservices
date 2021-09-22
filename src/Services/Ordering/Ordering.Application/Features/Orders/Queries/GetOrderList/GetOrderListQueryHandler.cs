23241c15a91f0a78a35dd75a88eebb22774e6671
Eduardo Medina <eduardo@soldsie.com>
2021-09-22 16:48:23 -0500

    Added: CheckoutOrderCommand, Handler, and Validator

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrderListQueryHandler : IRequestHandler<GetOrderListQuery, List<OrderDto>>
+    {
+
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrderListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrderDto>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+        { 
+            var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrderDto>>(orderList);
+        }
+    }
+}
